2636103ceb30a8cfc6bb6177093eed78b472d11d
Moshe Rosenstock <moshers@bu.edu>
2023-02-06 20:26:24 -0500

    feb-06-2023

---
+++ _build/jupyter_execute/docs/Final_Project_DS_121.py
@@ -0,0 +1,332 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <div style="text-align: right">B"H</div>
+# 
+# # DS 121 Final Project 
+# 
+# 
+# > ## *Moshe Rosenstock*
+# 
+# 
+
+# ## 1) Introduction:
+# 
+
+# - In this study, I used several machine learning algorithms to predict the level of alcohol in wines, based on its other attributes. I was interested in this problem because I wanted to know if it is possible to predict the alcohol level in a wine without actually measuring it, but rather by using the other factors that may influence its alcohol content. I divided the dataset into training and testing sets, and used kmeans clustering, hierarchical clustering, regression trees, and the Random Forest algorithm to make predictions on the test set.
+# 
+# - After calculating the different results of each of the algorithms, I will evaluated the performance  using mean absolute error and R^2. I will then compare the results of these metrics for each algorithm to determine which one was the best at predicting the alcohol level in wines.
+# 
+
+# In[1]:
+
+
+# Import Modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+import math
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from mpl_toolkits.mplot3d import Axes3D
+import plotly.graph_objs as go
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+from plotly import tools
+from plotly.subplots import make_subplots
+from sklearn.metrics import mean_absolute_error
+import plotly.offline as py
+from sklearn.preprocessing import MinMaxScaler
+
+
+# ## 2) Data:
+# - The dataset I utilized is based on a chemical analysis of wines grown in Italy. The study determined the amount of more than ten constituents found in each of the different types of wines.
+# 
+# - It was interesting to analyze this data as we could figure out a nice partitioning/classification of the level of alcohol in a wine using the other constituents found in the wine. The columns of this dataset are composed by the different components of wines, these includes Alcohol, Malic Acid, Ash, Ash_Alcanity, Magnesium, Total_Phenols, among others. On the other hand, each of the 178 rows represents a different wine, which all come from Italy's same region.
+# 
+# - These features give us a lot of space to make exciting classifications. One example to understand how our dataset works: it can show which component has the most significant correlation with the percentage of alcohol the wine has. 
+# 
+# - This work is essential because it can help winemakers to understand the factors that affect the alcohol level in their wines and to make more accurate predictions about the alcohol content of their wines. By using machine learning algorithms, winemakers can better understand the relationships between the attributes and can make more informed decisions about how to produce with the desired alcohol level. By predicting the alcohol level in a wine based on its other features, we can improve the accuracy and efficiency of the wine production process and ultimately produce better wines. Additionally, this work has broader implications for the wine industry, as it can help winemakers optimize their production processes and create high-quality wines tailored to their customer's preferences.
+# > This dataset was imported from Kaggle.com (Reference at the end)  
+# > https://www.kaggle.com/datasets/harrywang/wine-dataset-for-clustering 
+# 
+
+# In[7]:
+
+
+df = pd.read_csv("/Users/mosherosenstock/Desktop/final_project_ds121/wine-clustering.csv")
+
+
+# In[8]:
+
+
+df.head()
+
+
+# In[9]:
+
+
+# Scale all the columns to be between 0-1
+scaler = MinMaxScaler()
+df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+df.head()
+
+
+# In[10]:
+
+
+df.corr()
+
+
+# ## 3) Methodology: 
+# - In  this study, I used several machine learning algorithms to predict the level of alcohol in a wine based on its other attributes. I used clustering, hierarchical clustering, regression trees, and the Random Forest algorithm to make predictions on the test set.
+# Clustering is an unsupervised learning technique that is used to group similar data points into clusters. I used this algorithm to identify patterns or groupings in the data that may not be immediately apparent. And try to use these patterns to predict on the Test data.
+# Hierarchical clustering is a specific type of clustering algorithm that constructs a hierarchy of clusters by successively merging or splitting sets based on their similarity. I used this algorithm to further analyze the data patterns and to better understand the relationships between the different attributes. Although I know both cluster methods won't do as well as the regression methods, I still want to see how they react to this kind of task.
+# Regression trees are a supervised learning algorithm used for regression tasks, where the goal is to predict a continuous target variable based on a set of input features. I used regression trees to make predictions mainly because of their ability to subdivide data into many ranges, thus creating many leaves for the tree.
+# Lastly, I used the Random Forest algorithm, which is an ensemble learning method that combines multiple decision trees to make predictions. I chose this algorithm because it is typically more accurate and robust than a single decision tree, and it can handle high-dimensional data and large numbers of features more effectively. Most importantly, it is excellent at not overfitting, as each decision tree is trained on a different subset of the data.
+# After conducting all of these methods with my data, I will use three different scores to compare the algorithms' results. Even though regression and clustering algorithms cannot be ideally compared, I will still conduct the MSE, MAE, and R^2 to all the different algorithms. I will also conduct the silhouette score to both clustering methods, Kmeans and hierarchical, to see which of them is better with this data.
+# 
+# 
+# 
+# 
+# 
+
+# ## 4) Analysis:
+
+# In[12]:
+
+
+# creating a two dimentional matrix
+
+# Splitting my data into training and testing sets
+
+# We are dropping the 'smoker' column because I will use it for the Y_sample and test
+X=df.drop('Alcohol',axis='columns').values
+Y= df['Alcohol'].values
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+print(X.shape, X_train.shape, X_test.shape)
+
+
+# **Perform the Elbow Method to find optimal number of Clusters (We are going to do it with 2 different visualizations)**
+
+# In[10]:
+
+
+
+print("Visualization #1 of Elbow Method")
+WCSS = []
+for i in range(1,11):
+    model = KMeans(n_clusters = i,init = 'k-means++')
+    model.fit(X_train)
+    WCSS.append(model.inertia_)
+fig = plt.figure(figsize = (7,7))
+plt.plot(range(1,11),WCSS, linewidth=4, markersize=12,marker='o',color = 'b')
+plt.xticks(np.arange(11))
+plt.xlabel("Number of clusters")
+plt.ylabel("WCSS")
+plt.show()
+
+
+# In[14]:
+
+
+# create a KMeans model with 2 clusters. The reason Why I used two clusters, is because 2 gave me a better 
+# result using the mse,mae, and r^2 compared to using three clusters.
+kmeans = KMeans(n_clusters=2)
+
+kmeans.fit(X_train, Y_train)
+
+
+# In[17]:
+
+
+# Here I will messure the score of the clustering algorithm I just did
+
+
+p = kmeans.predict(X_test)
+
+sil = silhouette_score(X_test, p)
+mse1 = mean_squared_error(Y_test, p)
+mae1 = mean_absolute_error(Y_test, p)
+r1 = r2_score(Y_test, p)
+
+print('- The silhouette score is ', sil )
+print('- The R^2 is = ', r1)
+print('- The mean absolute error is', mae1)
+print('- The Mean Square Error =', mse1)
+
+
+# #### Hierarchial Clustering- (Different clustering algorithm)
+
+# In[39]:
+
+
+
+# create a hierarchical clustering model with 2 clusters
+#  (The reason why I used 2 cluster, instead of 3, is because when I use 3 in this type of clustering, 
+# I end up with a higher level of error.)
+hc = AgglomerativeClustering(n_clusters=2)
+
+
+hc.fit(X_train, Y_train)
+
+p2 = hc.fit_predict(X_test)
+
+sil2 = silhouette_score(X_test, p)
+mse2 = mean_squared_error(Y_test, p2)
+mae2 = mean_absolute_error(Y_test, p2)
+r2 = r2_score(Y_test, p2)
+
+
+print('- The silhouette score is =', sil2 )
+print('- The R^2 = ', r2)
+print('- The mean absolute error is =', mae2)
+print('- The Mean Square Error =',mse2)
+
+
+# ## Regression Trees
+
+# In[38]:
+
+
+
+dt = DecisionTreeRegressor()
+
+
+dt.fit(X_train, Y_train)
+
+# making the predictions on the test
+p3 = dt.predict(X_test)
+
+mse3 = mean_squared_error(Y_test, p3)
+mae3 = mean_absolute_error(Y_test, p3)
+r3 = r2_score(Y_test, p3)
+
+print('- The R^2 =', r3)
+print('- The mean absolute error is =', mae3)
+print('- The mean squared error is =' , mse3)
+
+
+# ## Random Forest
+
+# In[37]:
+
+
+
+p1 = RandomForestRegressor()
+p1.fit(X_train, Y_train)
+predictions = p1.predict(X_test)
+
+mse4 = mean_squared_error(Y_test, predictions)
+r4 = r2_score(Y_test, predictions)
+mae4 = mean_absolute_error(Y_test, predictions)
+
+
+print('- The R^2  = ', r4)
+print('- The mean absolute error  =', mae4)
+print('- The mean squared error  =', mse4)
+
+
+# **Plotting the R-Squared**
+
+# In[34]:
+
+
+import matplotlib.pyplot as plt
+
+r = [r1, r2,r3,r4]
+names = ['Clustering', 'Hierarchical Clustering',  'Regression Tree', 'Random Forest']
+# create the bar plot
+plt.bar(range(len(r)), r)
+plt.ylabel('Value')
+plt.title('R-Squared')
+plt.xticks(range(len(r)), names)
+plt.show()
+
+
+# **Mean Absolute Error**
+
+# In[35]:
+
+
+m = [mae1, mae2,mae3,mae4]
+names = ['Clustering', 'Hierarchical Clustering',  'Regression Tree', 'Random Forest']
+plt.bar(range(len(m)), m)
+plt.ylabel('Values')
+plt.title('Mean Absolute Error')
+plt.xticks(range(len(m)), names)
+plt.show()
+
+
+# **Mean Square Error**
+
+# In[36]:
+
+
+m = [mse1, mse2,mse3,mse4]
+names = ['Clustering', 'Hierarchical Clustering',  'Regression Tree', 'Random Forest']
+plt.bar(range(len(m)), m)
+
+plt.ylabel('Values')
+plt.title('Mean Square Error')
+plt.xticks(range(len(m)), names)
+plt.show()
+
+
+# ## 5) Results: 
+# 
+# After evaluating and working with different algorithms, I was able to find the best method to predict the level of alcohol in wines by considering its other chemical components. Although all of the algorithms can indicate in some way or another the alcohol level, it is worth mentioning that non of these methods will have a perfect result at predicting the desired variable. To get the precise measurement of each respective wine, a hydrometer or other scientific methods should be used.
+# Putting that information aside, the algorithms can give us a good approximation of the alcohol level.
+# 
+# - **Clustering:** Altough there are more practicals ways to measure and predict the alcohol level, I used two different algorithms to see what the results would be. I conducted two different clustering algorithms: K-means and Hierarchial clusterin. One of the differences between both of these algorithms is that K-means uses clusters, while hierarchical doesn't. As long as the distances between points and cluster centers are similar for both algorithms, they may have the same silhouette coefficient even if the clusters themselves are different. Although these clustering algorithms are somewhat different, they both got the same silhouette score, 0.343.
+# 
+# - **Regression:** The best way to predict a continuous target variable based on a set of input features is by using a regression algorithm. Therefore, I used two of the most potent regression algorithms: Regression Tree and Random Forest. Although both of these methods achieved a very good R-squared value, Mean Square Error, and Mean Absolute Error, the Random Forest algorithm ended up being the most efficient one. It got an MSE of .016, an MAE of .01, and an R^2 of .68, and when compared to the other used algorithms, these are very good numbers. One reason why random forests are better than a single regression tree is that they can reduce overfitting. Overfitting occurs when a model has learned the details of the training data too well and cannot generalize well to new data. Decision trees are prone to overfitting because they can keep splitting the data into smaller and smaller subgroups, and this can lead to a model that is highly complex and not able to generalize well. Random forests, on the other hand, can prevent overfitting by aggregating the predictions of multiple decision trees; which provides a more robust model that can generalize better to new data.
+
+# ## 6) Conclusion:  
+# To conclude, I divided my data into training and testing sets to prevent the overfitting of the models. Then, I used the training set to evaluate the performance of different algorithms in predicting the alcohol level of wines based on other attributes. After running each algorithm, I used various metrics to assess their performance. The results showed that the Random Forest algorithm was the most accurate, outperforming the other algorithms in the metric scores.
+# The ability to predict the alcohol level of a drink using other factors is valuable for several reasons. For example, it can help wine producers save time and money by reducing the need for hydrometer tests. It can also provide insights into the factors that influence the alcohol level of wines, allowing producers to optimize their production processes to achieve the desired alcohol level. Additionally, it can assist consumers in making informed decisions about the alcohol content of the wines they purchase. We can conclude that by using machine learning algorithms to predict the alcohol level of wines is a practical and valuable tool.
+
+# ## 7) References:  
+# All the links are accesible from the PDF format. (they are attached with hyperlinks)
+# 
+# 1. [Analizing the Results of Kmeans, Openclassrooms](https://openclassrooms.com/en/courses/5869986-perform-an-exploratory-data-analysis/6177861-analyze-the-results-of-a-k-means-clustering)
+# 
+# 2. [Kaggle, Creator: Narendia. Used as a base to clean the data.](https://www.kaggle.com/code/naren3256/kmeans-clustering-and-cluster-visualization-in-3d/notebook )
+# 
+# 4. [Guide on Kmeans Clustering](https://realpython.com/k-means-clustering-python/)  
+# 
+# 5. [Plotly Algorithms to visualize data](https://plotly.com/python/v3/ipython-notebooks/baltimore-vital-signs/)  
+# 
+# 6. [Plotly Algorithms to visualize data](https://plotly.com/python/ml-regression/)
+# 
+# 7. [Mean Square Error, By: Jim Frost](https://statisticsbyjim.com/regression/mean-squared-error-mse/)
+# 
+# 8. [How to calculate R squared, By Safa Mulani](https://www.digitalocean.com/community/tutorials/r-squared-in-r-programming)
+# 
+# 9. [Imported from SK-Learn, AgglomerativeClustering](https://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html)
+# 
+# 10. [Imported from SK-Learn, Mean Absolute Error](https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_absolute_error.html)
+# 
+# 11. [O’Reilly, Matplotlib Plots](oreilly.com/library/view/python-data-science/9781491912126/ch04.html)
+# 
+# 12. CDS DS 210 Lectures/Homeworks (Prof. Leonidas kontothanassis)
+# 
+# 13. [K-Means Clustering Algorithm, Towards Data Science](https://towardsdatascience.com/understanding-k-means-clustering-in-machine-learning-6a6e67336aa1)
+# 
+# 14. CDS DS 121 Lectures/Homeworks (Prof. Varia) (content from the HW was also made in collaboration with Classroom Peers)
+# 
+# 15. Colaborators: Jonathan Hafez (under syllabus policy)
+# 
---
+++ _build/jupyter_execute/docs/HW4_DS210.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # DS 210 Homework 4 
+# 
+# 
+# 
+
+# ## Decision Trees for regression
+# 
+# 
+
+# 3. (12 points) In this problem, you are asked to use decision trees for regression with two
+# different target loss functions: minimum square error and minimum absolute error.
+# Design a function f : [0, 1] → [0, 1] such that when you sample input points X𝑖 from [0, 1]
+# and use the sequence (X𝑖 , f(X𝑖)) as your input, you are likely to see the difference
+# between the two loss functions with six leafs (i.e., with max_leaf_nodes=6).
+# Among other things, your solution should contain:  
+# a. a plot of f and the two functions resulting from the training process under different
+# loss functions,  
+# b. an explanation of the differences and how they are a result of differences
+# between the two loss functions.  
+#   
+# Note: Make sure you read the documentation for sklearn.tree.DecisionTreeRegressor
+# including usage examples
+# https://scikit-learn.org/stable/auto_examples/tree/plot_tree_regression.html#sphx-glr-auto-examples-tree-plot-tree-regression-py
+
+# In[1]:
+
+
+# A
+import numpy as np
+import pandas as pd
+import random 
+from scipy.optimize import least_squares
+import math
+from sklearn.tree import DecisionTreeRegressor
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+# In[476]:
+
+
+# Import the necessary modules and libraries
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+# CREATE DATA (From [0,1])
+rng = np.random.RandomState(1)
+X = np.sort(1 * rng.rand(80, 1), axis=0)
+y = np.sin(X).ravel()
+y[::5] += 1 * (1.0 - rng.rand(16))
+
+
+# Fit regression model
+
+# Fit regression model 1
+# Decision Tree Regressor using "Squared Error" , to find the minimum square error
+regr_1 = DecisionTreeRegressor(criterion='squared_error',max_leaf_nodes=6,max_depth=6)
+
+
+# Fit regression model 2
+# Decision Tree Regressor using "absolute_error", to find the minimum absolute_error
+regr_2 = DecisionTreeRegressor(criterion='absolute_error',max_leaf_nodes=6, max_depth=6)
+
+
+regr_1.fit(X, y)
+regr_2.fit(X, y)
+
+'''If we would like to observe the visualization of the tree we could run this code below'''
+# print(tree.export_text(regr_1))
+# print(tree.export_text(regr_2))
+
+# Predict
+X_test = np.arange(0.0, 1.0, 0.01)[:, np.newaxis]
+y_1 = regr_1.predict(X_test)
+y_2 = regr_2.predict(X_test)
+
+
+
+# Plot the results
+plt.figure(figsize=(8,8))
+plt.scatter(X, y, s=20, edgecolor="black", c="darkorange", label="data")
+plt.plot(X_test, y_1, color="cornflowerblue", label="Squared Error", linewidth=2)
+plt.plot(X_test, y_2, color="yellowgreen", label="Absolute Error", linewidth=2)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Decision Tree Regression")
+plt.legend()
+plt.show()
+
+print("A) A plot of f and the two functions resulting from the training process" )
+
+
+# In[480]:
+
+
+#B
+print('B)\n')
+
+print('The differneces between the Minimum Squared Error and the Minimum Absolute Error (which are the two loss functions we used for this exersice), is that:\n\n- MAE minimizes the L1 loss using the each-terminal node and measures how close the predictions are to the outcomes. \n\n- However, MSE uses variance reduction as feature-selection criterion and minimizes the L2 loss using the mean of each terminal node; measuring the the difference between the estimator and estimated.')
+
+
+# ## Examples of Overfitting and Underfitting
+# 
+# 
+
+# In[564]:
+
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error
+import matplotlib
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[565]:
+
+
+# Set the random seed for reproducible results
+np.random.seed(42)
+
+# Generating function [0,1]
+def true_gen(x):
+    y = np.sin(1.2 * x * np.pi) 
+    return(y)
+
+# x values and y values
+x = np.sort(np.random.rand(120))
+y = true_gen(x) + 0.1 * np.random.randn(len(x))
+
+
+# In[567]:
+
+
+# Random indices for creating training and testing sets
+random_ind = np.random.choice(list(range(120)), size = 120, replace=False)
+xt = x[random_ind]
+yt = y[random_ind]
+
+# Training and testing observations
+train = xt[:int(0.7 * len(x))]
+test = xt[int(0.7 * len(x)):]
+
+y_train = yt[:int(0.7 * len(y))]
+y_test = yt[int(0.7 * len(y)):]
+
+# Model the true curve
+x_linspace = np.linspace(0, 1, 1000)
+y_true = true_gen(x_linspace)
+
+
+# In[529]:
+
+
+# Visualize observations and true curve
+plt.plot(train, y_train, 'ko', label = 'Train'); 
+plt.plot(test, y_test, 'ro', label = 'Test')
+plt.plot(x_linspace, y_true, 'b-', linewidth = 2, label = 'True Function')
+plt.legend()
+plt.xlabel('x'); plt.ylabel('y'); plt.title('Data');
+
+
+# In[523]:
+
+
+# Polynomial Fit Function 
+
+def fit_poly(train, y_train, test, y_test, degrees, plot='train', return_scores=False):
+    
+    # Create a polynomial transformation of features
+    features = PolynomialFeatures(degree=degrees, include_bias=False)
+    
+    # Reshape training features for use in scikit-learn and transform features
+    train = train.reshape((-1, 1))
+    train_trans = features.fit_transform(train)
+    
+    # Create the linear regression model and train
+    model = LinearRegression()
+    model.fit(train_trans, y_train)
+    
+    # Calculate the cross validation score
+    cross_valid = cross_val_score(model, train_trans, y_train, scoring='neg_mean_squared_error', cv = 5)
+    
+    # Training predictions and error
+    train_predictions = model.predict(train_trans)
+    training_error = mean_squared_error(y_train, train_predictions)
+    
+    # Format test features
+    test = test.reshape((-1, 1))
+    test_trans = features.fit_transform(test)
+    
+    # Test set predictions and error
+    test_predictions = model.predict(test_trans)
+    testing_error = mean_squared_error(y_test, test_predictions)
+    
+    # Find the model curve and the true curve
+    x_curve = np.linspace(0, 1, 100)
+    x_curve = x_curve.reshape((-1, 1))
+    x_curve_trans = features.fit_transform(x_curve)
+    
+    # Model curve
+    model_curve = model.predict(x_curve_trans)
+    
+    # True curve
+    y_true_curve = true_gen(x_curve[:, 0])
+    
+    # Plot observations, true function, and model predicted function
+    if plot == 'train':
+        plt.plot(train[:, 0], y_train, 'ko', label = 'Observations')
+        plt.plot(x_curve[:, 0], y_true_curve, linewidth = 4, label = 'True Function')
+        plt.plot(x_curve[:, 0], model_curve, linewidth = 4, label = 'Model Function')
+        plt.xlabel('x'); plt.ylabel('y')
+        plt.legend()
+        plt.ylim(-1, 1.5); plt.xlim(0, 1)
+        plt.title('{} Degree Model on Training Data'.format(degrees))
+        plt.show()
+        
+    elif plot == 'test':
+        # Plot the test observations and test predictions
+        plt.plot(test, y_test, 'o', label = 'Test Observations')
+        plt.plot(x_curve[:, 0], y_true_curve, 'b-', linewidth = 2, label = 'True Function')
+        plt.plot(test, test_predictions, 'ro', label = 'Test Predictions')
+        plt.ylim(-1, 1.5); plt.xlim(0, 1)
+        plt.legend(), plt.xlabel('x'), plt.ylabel('y'); plt.title('{} Degree Model on Testing Data'.format(degrees)), plt.show();
+    
+    # Return the metrics
+    if return_scores:
+        return training_error, testing_error, -np.mean(cross_valid)
+    
+
+
+# ## Example of Underfitting
+#   * (When we use Degrees = 1)  
+#   
+# **The model Clearly underfits the function, as the Model data is not even close to the True function. As we can appreciate in the graphs below**
+
+# In[524]:
+
+
+fit_poly(train, y_train, test, y_test, degrees = 1, plot='train')
+
+
+# In[525]:
+
+
+fit_poly(train, y_train, test, y_test, degrees = 1, plot='test')
+
+
+# ## Example of Overfitting
+#   * (When we use Degrees = 25)  
+# 
+# **The model Clearly Overfits the function, as the Model data is has strange curves that cross the true function constantly. As we can appreciate in the graphs below**
+
+# In[526]:
+
+
+fit_poly(train, y_train, test, y_test, plot='train', degrees = 25)
+
+
+# In[527]:
+
+
+fit_poly(train, y_train, test, y_test, degrees=25, plot='test')
+
+
+# ## Example of a Balanced Model
+#   * (When we use Degrees = 5)  
+#   
+# **The model fits correctly the function, as the Model data is almost the same as the True Function. Which means our training model has a correct result. As we can appreciate in the graphs below**
+
+# In[546]:
+
+
+fit_poly(train, y_train, test, y_test, plot='train', degrees = 5)
+
+
+# In[568]:
+
+
+fit_poly(train, y_train, test, y_test, degrees=5, plot='test')
+
+
+# ## Here is another good Visualization of UnderFitting / Overfitting 
+# (imported from scikit-learn.org )
+
+# In[179]:
+
+
+# 
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+
+def true_fun(X):
+    return np.cos(1.5 * np.pi * X)
+
+
+np.random.seed(0)
+
+n_samples = 30
+degrees = [1, 4, 15]
+
+X = np.sort(np.random.rand(n_samples))
+y = true_fun(X) + np.random.randn(n_samples) * 0.1
+
+plt.figure(figsize=(14, 5))
+for i in range(len(degrees)):
+    ax = plt.subplot(1, len(degrees), i + 1)
+    plt.setp(ax, xticks=(), yticks=())
+
+    polynomial_features = PolynomialFeatures(degree=degrees[i], include_bias=False)
+    linear_regression = LinearRegression()
+    pipeline = Pipeline(
+        [
+            ("polynomial_features", polynomial_features),
+            ("linear_regression", linear_regression),
+        ]
+    )
+    pipeline.fit(X[:, np.newaxis], y)
+
+    # Evaluate the models using crossvalidation
+    scores = cross_val_score(
+        pipeline, X[:, np.newaxis], y, scoring="neg_mean_squared_error", cv=10
+    )
+
+    X_test = np.linspace(0, 1, 100)
+    plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
+    plt.plot(X_test, true_fun(X_test), label="True function")
+    plt.scatter(X, y, edgecolor="b", s=20, label="Samples")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.xlim((0, 1))
+    plt.ylim((-2, 2))
+    plt.legend(loc="best")
+    plt.title('underfitting')#,'regular fitting','overfitting')
+    plt.title(
+        "Degree {}\nMSE = {:.2e}(+/- {:.2e})".format(
+            degrees[i], -scores.mean(), scores.std()
+        )
+    
+    )
+plt.show()
+
+print(" -From this code we can clearly see the difference between underfitting (which is the graph of the left) \nand overfitting (which is the graph of the right)")
+print(" -We can see how the function of the model changes according to the amount \nof features and expressivness of the problem")
+
+
+# #### From the graphs plotted above, we can appreciate 3 clear examples:
+# - The graph of the left (that uses only one degree) is a clear example of UnderFitting. The Model Function is not close to the True function.  
+# - The Center graph is an example of a balanced model (it uses 4 degrees), as the Model Function is almost identical to the True function. 
+# - The graph of the left is an example of Overfitting (it uses 15 degrees), as we can appreciate those strange curves of the model and how they differentiate from the True function.
